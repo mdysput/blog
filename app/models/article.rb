0e35dfc675f968c33186da550bb08fd0f0881a89
Michal <michal.dysput@gmail.com>
2019-04-17 12:17:54 +0200

    New action and routes

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,6 @@
 class Article < ActiveRecord::Base
 
-  validates :title, presence: true, lenght: { minimum: 3, maximum: 50}
-  valdidates :description, presence: true, lenght: { minimum: 10, maximum: 200}
+  validates :title, presence: true
+  validates :description, presence: true
 
 end
