6582bb32401d93ee1941a038e904b9da77814082
Michal <michal.dysput@gmail.com>
2019-05-03 15:05:01 +0200

    Errors added'

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -18,7 +18,7 @@ class ArticlesController < ApplicationController
 
     @article = Article.new(article_params)
     if @article.save
-      flash[:notice] = "Article was successfully created"
+      flash[:success] = "Article was successfully created"
       redirect_to article_path(@article)
     else
       render 'new'
@@ -28,7 +28,7 @@ class ArticlesController < ApplicationController
   def update
 
     if @article.update(article_params)
-      flash[:notice]= "Article was successfully updated"
+      flash[:danger]= "Article was successfully updated"
       redirect_to article_path(@article)
     else
       render 'edit'
