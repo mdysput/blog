fcba6866ee006f22c212e10ec1f0e464615fce53
Michal <michal.dysput@gmail.com>
2019-05-04 13:20:08 +0200

    Add BlogUser validations

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,7 @@
 class ArticlesController < ApplicationController
 
   before_action :set_article, only: [:edit, :update, :show, :destroy]
+  # @article = Article.find(params[:id])
 
   def index
     @articles = Article.all
--- app/models/blog_user.rb
+++ app/models/blog_user.rb
@@ -1,3 +1,9 @@
 class BlogUser < ApplicationRecord
+  validates :name, presence: true, uniqueness: true, length: { minimum: 3, maximum: 25}
+  VALID_EMAIL_REGEX=  /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true,
+            length: { minimum: 10, maximum: 30},
+            uniqueness: { case_sensitive: false},
+            format: { with: VALID_EMAIL_REGEX}
 
 end
