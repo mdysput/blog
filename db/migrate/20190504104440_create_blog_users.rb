b63b36da2e35d353d469d5cb351abc5a29a7e46b
Michal <michal.dysput@gmail.com>
2019-05-05 18:31:06 +0200

    Refactor big mistake

--- db/migrate/20190504104440_create_blog_users.rb
+++
@@ -1,9 +0,0 @@
-class CreateBlogUsers < ActiveRecord::Migration[5.2]
-  def change
-    create_table :blog_users do |t|
-      t.string :name
-      t.string :email
-      t.timestamps
-    end
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,20 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_04_104440) do
+ActiveRecord::Schema.define(version: 2019_05_04_101729) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.datetime "created_at"
     t.datetime "updated_at"
-  end
-
-  create_table "blog_users", force: :cascade do |t|
-    t.string "name"
-    t.string "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
